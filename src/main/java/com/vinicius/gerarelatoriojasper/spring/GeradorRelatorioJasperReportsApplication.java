8655f6ba57404457c4f66b56689b09aadf55db41
Vinicius Pascucci <vinicius.pascucci1@gmail.com>
2019-10-23 19:14:30 -0300

    Configurando acesso ao banco de dados

---
+++ src/main/java/com/vinicius/gerarelatoriojasper/spring/GeradorRelatorioJasperReportsApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.gerarelatoriojasper.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorRelatorioJasperReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorRelatorioJasperReportsApplication.class, args);
+	}
+
+}
