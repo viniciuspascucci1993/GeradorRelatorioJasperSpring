c16e10c50f2e8d886c6a2b5a154c960f9db0d410
Vinicius Pascucci <vinicius.pascucci1@gmail.com>
2019-10-23 22:43:51 -0300

    Finalizando a geração de relatorios em PDF com Jasper Reports

--- src/main/java/com/vinicius/gerarelatoriojasper/spring/controller/ProdutoController.java
+++ src/main/java/com/vinicius/gerarelatoriojasper/spring/controller/ProdutoController.java
@@ -3,6 +3,7 @@ package com.vinicius.gerarelatoriojasper.spring.controller;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,7 +15,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import net.sf.jasperreports.engine.JREmptyDataSource;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
@@ -34,23 +34,30 @@ public class ProdutoController {
 	private DataSource dataSource;
 	
 	@RequestMapping( value = "/relatorios", method = RequestMethod.GET)
-	public void imprimirRelatorio( HttpServletResponse response) throws JRException {
+	public void imprimirRelatorio( HttpServletResponse response) throws JRException, SQLException {
 		
 		InputStream reportStream = this.getClass().getResourceAsStream("/relatorios/Produtos.jasper");
 		
 		Map<String, Object> params = new HashMap<String, Object>();
 		
+		// Outra maneira de se trabalhar com relatorios utilizando jasper é usar o HashMap params.
+//		params.put("PARAM_1", "CUSTOM PARAM");
+		
+		// O trecho abaixo é o necessarios para geração de relatorio em formato PDF.
+		// Faço a captura dos meu item(JasperReports)
 		JasperReport jasperReport = ( JasperReport ) JRLoader.loadObject(reportStream);
 		
-		JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, new JREmptyDataSource());
+		//Preencho com os meus dados.
+		JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, dataSource.getConnection());
 		
+		// Altero a minha resposta
 		response.setContentType("application/pdf");
-		
 		response.setHeader("Content-Disposition", "inline; filename=produtos_games.pdf");
 		
 		try {
+			//Em um bloco try/catch finalizo com OutPutStream
+			//Obs: poderia usar ByteArrayOutPutStream também
 			OutputStream outputStream = response.getOutputStream();
-			
 			JasperExportManager.exportReportToPdfStream(jasperPrint, outputStream);
 			
 		} catch (IOException e) {
